41f7ce8593fc3efa7e61e4521cb3dd0f62bc1af1
fkuca <fkuca91@gmail.com>
2022-01-14 23:09:57 +0100

    feat(layout): add Page component

--- src/layout/Page.jsx
+++ src/layout/Page.jsx
@@ -1,5 +1,8 @@
+import { Wrapper } from './components/Wrapper';
 import styles from './page.module.css';
 
 export const Page = ({ children }) => (
-	<main className={styles.page}>{children}</main>
+	<main className={styles.page}>
+		<Wrapper>{children}</Wrapper>
+	</main>
 );
