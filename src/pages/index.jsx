4456a35bc94c592460fc1e4133057bfcec19a0e3
fkuca <fkuca91@gmail.com>
2022-01-15 13:21:25 +0100

    refactor(main): change Main component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,1 +1,15 @@
-export const NotFound = () => <div>Not found</div>;
+import { Section } from '../components/section/Section';
+import { Actions } from '../components/actions/Actionx';
+import { Button } from '../components/button/Button';
+
+export const NotFound = () => (
+	<Section sectionHeader='Not found'>
+		Oops! Sorry. This page does not exist. Click below to go back to the main
+		screen.
+		<Actions>
+			<Button isLink to='/'>
+				Go back
+			</Button>
+		</Actions>
+	</Section>
+);
