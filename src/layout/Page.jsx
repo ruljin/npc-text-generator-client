1709efc0bdef9f88288af48eb995e3f5abfe2698
fkuca <fkuca91@gmail.com>
2022-01-15 13:20:37 +0100

    refactor(layout): change Page and Wrapper components

--- src/layout/Page.jsx
+++ src/layout/Page.jsx
@@ -1,8 +1,10 @@
-import { Wrapper } from './components/Wrapper';
+import { Wrapper } from './components/wrapper/Wrapper';
+import { Footer } from './components/footer/Footer';
 import styles from './page.module.css';
 
 export const Page = ({ children }) => (
 	<main className={styles.page}>
 		<Wrapper>{children}</Wrapper>
+		<Footer />
 	</main>
 );
--- src/layout/components/Wrapper.jsx
+++
@@ -1,5 +0,0 @@
-import styles from './wrapper.module.css';
-
-export const Wrapper = ({ children }) => (
-	<div className={styles.wrapper}>{children}</div>
-);
