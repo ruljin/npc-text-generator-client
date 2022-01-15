e734fe48e94c90d420390c6fda1827e6d3bd4d18
fkuca <fkuca91@gmail.com>
2022-01-15 13:20:57 +0100

    refactor(section): change Section component

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -3,10 +3,10 @@ import styles from './section.module.css';
 export const Section = ({
 	isWelcomeSection = false,
 	sectionHeader = '',
-	sectionContent = '',
+	children,
 }) => (
 	<section className={styles.section}>
 		{isWelcomeSection ? <h1>{sectionHeader}</h1> : <h2>{sectionHeader}</h2>}
-		<p>{sectionContent}</p>
+		<p className={styles.section__content}>{children}</p>
 	</section>
 );
