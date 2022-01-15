c3fedb812decec4bdd24c5fa0cab83a5767df1a0
fkuca <fkuca91@gmail.com>
2022-01-15 15:47:06 +0100

    feat(select): add Select component

---
+++ src/components/select/Select.jsx
@@ -0,0 +1,22 @@
+import { forwardRef } from 'react';
+import styles from './select.module.css';
+
+export const Select = forwardRef(
+	({ label, onChange, onBlur, name, options }, ref) => (
+		<label className={styles.label}>
+			{label}
+			<select
+				className={styles.select}
+				ref={ref}
+				name={name}
+				onChange={onChange}
+				onBlur={onBlur}>
+				{options.map((option) => (
+					<option key={option} value={option.id}>
+						{option.value}
+					</option>
+				))}
+			</select>
+		</label>
+	)
+);
