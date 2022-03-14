a7cdc0778408d60dc21cc5d60bcce3e570ae3f1e
fkuca <fkuca91@gmail.com>
2022-03-14 16:02:58 +0100

    feature(modal): add Modal component

--- src/modules/generator/pages/Generator.jsx
+++ src/modules/generator/pages/Generator.jsx
@@ -1,1 +1,20 @@
-export const Generator = () => <div>Generator</div>;
+import { useModalContext } from '../../../components/modal/hooks/useModalContext';
+import { GossipModal } from '../modals/GossipModal';
+
+export const Generator = () => {
+	const { open, setOpen } = useModalContext();
+	console.log({ open, setOpen });
+
+	return (
+		<div>
+			Generator
+			{open && <GossipModal />}
+			<button
+				onClick={() => {
+					setOpen(!open);
+				}}>
+				Open modal
+			</button>
+		</div>
+	);
+};
